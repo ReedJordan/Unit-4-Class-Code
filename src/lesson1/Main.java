fc58025b42219afedb5d0199f30ef3c9d6c04384
jordanr_wlhs <jordanr@wlhs.wlwv.k12.or.us>
2024-10-31 14:29:36 -0700

    lesson 1 complete

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -1,4 +1,5 @@
 package lesson1;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,6 +8,17 @@ public class Main {
     }
 
     public static void guessMyNumber(){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("See if you can guess the number!");
+        int guess = input.nextInt();
+
+        while (guess != 4){
+            System.out.println("Try again!");
+            guess = input.nextInt();
+        }
+
+        System.out.println("Congratulations, you did it!");
 
     }
 }
