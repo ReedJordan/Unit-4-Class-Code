017ceff1b7589a99072c2716653e9cb5d4f9eb92
jordanr_wlhs <jordanr@wlhs.wlwv.k12.or.us>
2024-11-14 14:56:32 -0800

    done homework, added lesson 2

--- src/homework1/Unit4HW1.java
+++ src/homework1/Unit4HW1.java
@@ -1,11 +1,14 @@
 package homework1;
+import java.util.Random;
+import java.util.Scanner;
+
 
 public class Unit4HW1 {
 
     public static void main(String[] args) {
-        guessingGame();
-        tiredTurtle();
-        notATamagotchi();
+        //guessingGame();
+        //tiredTurtle();
+        tomaNOTchi();
 
     }
 
@@ -16,7 +19,22 @@ public class Unit4HW1 {
      */
     public static void guessingGame() {
         //feel free to use the Random class
-        System.out.println("To be done...");
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+        int pickMe = random.nextInt(0, 100);
+        System.out.println("I have a random number, between 1 and 100! Can you guess what it is?");
+        System.out.println("Enter your number: ");
+        int userGuess = input.nextInt();
+        while (userGuess != pickMe) {
+            if (userGuess > pickMe) {
+                System.out.print("Your guess is too high! ");
+            } else {
+                System.out.print("Your guess is too low! ");
+            }
+            System.out.println("Guess again: ");
+            userGuess = input.nextInt();
+        }
+        System.out.println("Hooray! You got it!");
     }
 
     /**
@@ -26,15 +44,50 @@ public class Unit4HW1 {
      * the turtle took until it stopped.
      */
     public static void tiredTurtle() {
-        System.out.println("To be done...");
+        Scanner turt = new Scanner(System.in);
+        System.out.println("This turtle may be tired, but it has to move. How many steps should it take?");
+        int stepChoice = turt.nextInt();
+        int moveNum = 0;
+        int moveTot = 0;
+        int numSteps = stepChoice;
+
+        while (numSteps > 1){
+            moveNum ++;
+            System.out.println("Move "+ moveNum +": "+ numSteps +" steps.");
+            moveTot += numSteps;
+            numSteps = (int)(numSteps/2);
+        }
+
+        System.out.println("\nFirst move: "+ stepChoice);
+        System.out.println("Total steps: "+ moveTot);
     }
 
-
-    /**
-     * Write your own description
-     */
-    public static void notATamagotchi() {
-        System.out.println("To be done...");
+    public static void tomaNOTchi() {
+        Scanner toma = new Scanner(System.in);
+        int hourNum = 0;
+        int hungLevel = 10;
+        String feedIt = "no";
+        System.out.println("Hour 0: Current hunger level 0.");
+        while (hourNum <= 3){
+            hourNum++;
+            System.out.print("Hour "+ hourNum +": Current hunger level is "+ hungLevel +". ");
+            System.out.print("Do you want to feed your pet? (yes/no) ");
+            feedIt = toma.nextLine();
+            if (feedIt.equals("yes")){
+                if (hungLevel <= 25){
+                    hungLevel = 0;
+                }else if (hungLevel > 25){
+                    hungLevel -= 25;
+                }
+            }
+            hungLevel += 10;
+        }
+        String petStatus = "content";
+        if (hungLevel >= 40){
+            petStatus = "dead";
+        }
+        System.out.println("Hour "+ (hourNum+1) +": Current hunger level is "+ hungLevel +". ");
+        System.out.print("End of simulation. Your pet is "+ petStatus +". ");
     }
 
 }
---
+++ src/lesson2/Main.java
@@ -0,0 +1,19 @@
+package lesson2;
+
+public class Main {
+    public static void main(String[]args){
+
+
+        for(int start = 0; start < 10; start++){
+            System.out.println(start);
+        }
+
+        int start2 = 0;
+        while (start2 < 10){
+            System.out.println(start2);
+            start2++;
+        }
+
+        //these things are always interchangeable
+    }
+}
