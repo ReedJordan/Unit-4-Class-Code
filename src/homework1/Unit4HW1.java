a056753d392e06142150b5c40732128d3f1bb929
jordanr_wlhs <jordanr@wlhs.wlwv.k12.or.us>
2024-11-14 14:01:35 -0800

    added to lesson 1

--- src/homework1/Unit4HW1.java
+++ src/homework1/Unit4HW1.java
@@ -13,9 +13,8 @@ public class Unit4HW1 {
      * Picks a random number between 1 and 100 and asks the user
      * to guess it. For each guess, the program should tell the user whether
      * the guess is too high, too low, or correct.
-     *
      */
-    public static void guessingGame(){
+    public static void guessingGame() {
         //feel free to use the Random class
         System.out.println("To be done...");
     }
@@ -25,18 +24,16 @@ public class Unit4HW1 {
      * Asks the user how many steps they want the turtle to take in its
      * first move. Then it calculates and displays how many total steps
      * the turtle took until it stopped.
-     *
      */
-    public static void tiredTurtle(){
+    public static void tiredTurtle() {
         System.out.println("To be done...");
     }
-
 
 
     /**
      * Write your own description
      */
-    public static void notATamagotchi(){
+    public static void notATamagotchi() {
         System.out.println("To be done...");
     }
 
--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -1,25 +1,40 @@
 package lesson1;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         guessMyNumber();
+        Scanner scan = new Scanner(System.in);
 
-    }
 
-    public static void guessMyNumber(){
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("See if you can guess the number!");
-        int guess = input.nextInt();
-
-        int secretNum = (int)(Math.random()*10+1);
-        while (guess != secretNum){
-            System.out.println("Try again!");
-            guess = input.nextInt();
+        int startValue = 0;
+        while (startValue < 10) {
+            System.out.println(startValue);
+            startValue++;
         }
 
-        System.out.println("Congratulations, you did it!");
+        while (true) {
+            System.out.println("Enter a number: ");
+            int userNum = scan.nextInt();
+            if (userNum == 8) {
+                break;
+            }
+        }
 
+        public static void guessMyNumber() {
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("See if you can guess the number!");
+            int guess = input.nextInt();
+
+            int secretNum = (int) (Math.random() * 10 + 1);
+            while (guess != secretNum) {
+                System.out.println("Try again!");
+                guess = input.nextInt();
+            }
+
+            System.out.println("Congratulations, you did it!");
+
+        }
     }
-}
