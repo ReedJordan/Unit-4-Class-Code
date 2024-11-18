a9b4a9fa5bdbd5a228341d53488d61c2283130a5
ReedJordan <89485455+ReedJordan@users.noreply.github.com>
2024-11-18 14:56:44 -0800

    vid 1 done

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -5,6 +5,23 @@ The stuff that I need to add after watching the videos.
  */
 public class Main {
     public static void main(String[] args){
-        
+        substringFinder()
+    }
+    public class substringFinder(){
+
+        String word = "sunflower";
+        String sub = "lower";
+        boolean found = false;
+        for (int i = 0; i <= word.length() - sub.length(); i++){
+            String portion = word.substring(i, i + sub.length());
+            if (portion.equals(sub)){
+                found = true;
+            }
+        }
+        if (found){
+            System.out.println("We found the smaller String!");
+        }else{
+            System.out.println("We did not find the smaller String.");
+        }
     }
 }
