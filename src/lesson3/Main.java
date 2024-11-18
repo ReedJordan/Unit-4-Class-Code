b3af0b610a2168a6737ad1ec75323ba89c2cc12d
ReedJordan <89485455+ReedJordan@users.noreply.github.com>
2024-11-18 15:05:33 -0800

    Update Main.java

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -5,9 +5,11 @@ The stuff that I need to add after watching the videos.
  */
 public class Main {
     public static void main(String[] args){
-        substringFinder()
+        substringFinder();
+        substringNumbers();
     }
-    public class substringFinder(){
+    
+    public static void substringFinder(){
 
         String word = "sunflower";
         String sub = "lower";
@@ -24,4 +26,19 @@ public class Main {
             System.out.println("We did not find the smaller String.");
         }
     }
+
+    public static void substringNumbers(){
+        String word = "supercalifragilisticexpialidocious";
+        int count = 0;
+        for (int i = 0; i < word.length(); i++){
+            String letter = word.substring(i, i+1);
+            if (letter.equals("a") || letter.equals("e") || letter.equals("i") || 
+                letter.equals("o") || letter.equals("u")){
+
+                count++;
+            }
+        System.out.println("The number of vowels in "+ word +" is "+ count);
+    }
+
+
 }
