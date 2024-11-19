ae4e07a67c30a5ab48a71e293509d822c2953f1d
ReedJordan <89485455+ReedJordan@users.noreply.github.com>
2024-11-18 16:25:39 -0800

    donezo

--- src/lesson4/Main.java
+++ src/lesson4/Main.java
@@ -1,10 +1,50 @@
 package lesson4;
 
-/*
-More stuff to add after videos
- */
+
 public class Main {
     public static void main(String[] args){
-
+    doubleForLoops();
     }
+
+    public static void doubleForLoops(){
+        
+        for (int outer = 1; outer < 5; outer++){
+            for (int inner = 1; inner < 3; inner ++){
+                System.out.print(inner + " ");
+            }
+            System.out.println();
+        }
+        
+        
+        for (int outer = 1; outer < 5; outer++){
+            for (int inner = 1; inner <= outer; inner++){
+            System.out.print("*");
+            }
+        System.out.println();
+        }
+
+        for(int outer = 4; outer >= 1; outer--){
+            for (int inner = outer; inner > 0; inner--){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+
+        
+        //Double While loops:
+        int result = 0;
+        int k = 0;
+        while(k < 4){
+            int m = 0;
+            while (m < 3){
+                result++;
+                m++
+            }
+        k++;
+        }
+        System.out.println(result);
+        
+        
+    }
+
 }
