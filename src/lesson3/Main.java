93c6e2c310c283c8ebc3299ef34bbfcc20630ac8
jordanr_wlhs <jordanr@wlhs.wlwv.k12.or.us>
2024-11-20 13:41:15 -0800

    file editing

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -2,6 +2,7 @@ package lesson3;
 
 /*
 The stuff that I need to add after watching the videos.
+Developing algorithms using strings
  */
 public class Main {
     public static void main(String[] args){
